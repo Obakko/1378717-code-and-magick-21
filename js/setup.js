66c348d6c02254f79daa23c213ad638284d9ea4c
Obakko <romansaburski36@gmail.com>
2020-10-05 12:02:04 +0300

    'поправки'

--- js/setup.js
+++ js/setup.js
@@ -1,26 +1,26 @@
 'use strict';
-const nameWizard = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
-const surnameWizard = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
-const coatColor = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
-const eyesColor = [`black`, `red`, `blue`, `yellow`, `green`];
+const NAME_WIZARD = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
+const SURNAME_WIZARD = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
+const COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+const EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
 const similarListElement = document.querySelector(`.setup-similar-list`);
 const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
     .content
     .querySelector(`.setup-similar-item`);
 const userDialog = document.querySelector(`.setup`);
+const NUMBER_WIZARDS = 4;
 
-const getRandom = (array) => {
-  return array[Math.floor(Math.random() * array.length)];
+const getRandomValueArray = (valueArray) => {
+  return valueArray[Math.floor(Math.random() * valueArray.length)];
 };
 const getArrayWizards = () => {
-  let arrayWizards = [];
-  for (let i = 0; i < 4; i++) {
-    let objectForWizards = {
-      name: getRandom(nameWizard) + ` ` + getRandom(surnameWizard),
-      coatColor: getRandom(coatColor),
-      eyesColor: getRandom(eyesColor)
-    };
-    arrayWizards.push(objectForWizards);
+  const arrayWizards = [];
+  for (let i = 0; i < NUMBER_WIZARDS; i++) {
+    arrayWizards.push({
+      name: getRandomValueArray(NAME_WIZARD) + ` ` + getRandomValueArray(SURNAME_WIZARD),
+      coatColor: getRandomValueArray(COAT_COLOR),
+      eyesColor: getRandomValueArray(EYES_COLOR)
+    });
   }
   return arrayWizards;
 };
